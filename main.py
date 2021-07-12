82185748156f530bccbdd2a524e611062b019f18
wilgardner <w.gardner@latrobe.edu.au>
2021-07-12 23:35:09 +1000

    Added image to HTML template. Added index.html and edge finder.

--- main.py
+++ main.py
@@ -1,32 +1,67 @@
-from fastapi import FastAPI, Request, Form, UploadFile, Query
+from fastapi import FastAPI, Request, Form, UploadFile, Query, HTTPException
 from fastapi.responses import Response
 from fastapi.templating import Jinja2Templates
 from PIL import Image, ImageFilter
 from io import BytesIO
+import base64
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
 
 @app.get("/")
-def form(request: Request):
-    response = "Load new image and select a smoothing radius"
-    return templates.TemplateResponse('form.html', context={'request': request, 'response': response})
+async def root(request: Request):
+    return templates.TemplateResponse('index.html', context={'request': request})
 
 
-@app.post("/smoothimage/")
-async def smooth_image(
+@app.get("/blurimage/")
+async def blur_image_form(request: Request):
+    msg = "Load an image and select a blur radius, then click Blur!"
+    return templates.TemplateResponse('blurrer.html', context={'request': request, 'message': msg, 'Result': ""})
+
+
+@app.post("/blurimage/")
+async def blur_image(
         request: Request,
         img_file: UploadFile = Form(...),
         blur_radius: int = Form(1, gt=0)):
+    # Validate file type
+    if img_file.content_type.split('/')[0] != 'image':
+        raise HTTPException(400, detail="Invalid file type")
     # Open image as PIL Image
-    img = Image.open(img_file.file) # Should I be handling exceptions here (e.g. wrong file type)?
-    # Smooth the image
-    img_smoothed = img.filter(ImageFilter.GaussianBlur(radius=blur_radius))
+    img = Image.open(img_file.file)
+    # Blur the image
+    img_blurred = img.filter(ImageFilter.GaussianBlur(radius=blur_radius))
     temp_file = BytesIO()
-    img_smoothed.save(temp_file, format="PNG")
-    response = Response(content=temp_file.getvalue(), media_type="image/png")
-    return response
-    # return templates.TemplateResponse('form.html', context={'request': request, 'response': response})
+    img_blurred.save(temp_file, format="PNG")
+    img_url = base64.b64encode(temp_file.getvalue()).decode()
+    result = "<img src='data:image/png;base64,{}'>".format(img_url)
+    # Message
+    msg = "Blurred {}, using a blur radius of {}:".format(img_file.filename, blur_radius)
+    return templates.TemplateResponse('blurrer.html', context={'request': request, 'message': msg, 'result': result})
 
-    # QUESTION: How do I send the response within the HTML format and show the image on the webpage???
+
+@app.get("/findedges/")
+def find_edges_form(request: Request):
+    msg = "Load an image, then click Find Edges!"
+    return templates.TemplateResponse('edge_finder.html', context={'request': request, 'message': msg, 'Result': ""})
+
+
+@app.post("/findedges/")
+async def find_edges(
+        request: Request,
+        img_file: UploadFile = Form(...)):
+    # Validate file type
+    if img_file.content_type.split('/')[0] != 'image':
+        raise HTTPException(400, detail="Invalid file type")
+    # Open image as PIL Image
+    img = Image.open(img_file.file)
+    # Blur the image
+    img_blurred = img.filter(ImageFilter.FIND_EDGES)
+    temp_file = BytesIO()
+    img_blurred.save(temp_file, format="PNG")
+    img_url = base64.b64encode(temp_file.getvalue()).decode()
+    result = "<img src='data:image/png;base64,{}'>".format(img_url)
+    # Message
+    msg = "Edges found for {}:".format(img_file.filename)
+    return templates.TemplateResponse('edge_finder.html', context={'request': request, 'message': msg, 'result': result})
